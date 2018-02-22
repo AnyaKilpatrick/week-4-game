0690112bc67dcbb3c7507d5a2ce6ee93b6a46546
AnyaKilpatrick <hannakilpatrick@gmail.com>
2018-02-22 00:40:10 -0500

    Added js content

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,16 @@
-// Global Variables
-// var myCharacter
-// var myImage
-// var myBody
-// var myName
-// var myHp
-// var defender
-// var defImage
-// var defBody
-// var defName
-// var defHp
-
 // Character Object
 var characters = {
     "Lord Voldemort": {
         "Health Points": 180,
         "Attack Power": 20,
-        "Counter Attack Power": 0
     },
     "Hermione Granger": {
         "Health Points": 100,
         "Attack Power": 15,
-        "Counter Attack Power": 0
     },
     "Sirius Black": {
         "Health Points": 120,
         "Attack Power": 18,
-        "Counter Attack Power": 0
     },
     "Bellatrix Lestrange": {
         "Health Points": 160,
@@ -57,9 +42,22 @@ var pickedDefender = false;
 var character
 var defender
 
+var myCharacterName
+var myCharacterHpHolder
+var myDefenderName
+var myDefenderHpHolder
+var myHp
+var defenderHp
+var myAttack
+var myGrowPower
+var defenderAttack
+var myGrowPower
+
 $(document).ready(function(){
+
 // Picking User Character
     $(".character").on("click", function (){
+        
         // Picking a Character
         if (pickedCharacter === false) {
             character = $(this); //grabbing a clicked div
@@ -67,10 +65,14 @@ $(document).ready(function(){
             character.remove(); //remove it from original location
             pickedCharacter = true; //set var pickedCharacter to "true"
             character.removeClass("character").addClass("myCharacter"); //changing class name for our picked character
+            
 
             $("#pickedUser").append(character); // insert removed div to "You Character" div
             $(".character").removeClass("character").addClass("defender"); //changing class name of left divs, so we can treat them differently later and set different style
             $("#pick").text("Pick Your Defender");
+
+            
+            
         }
         // Picking a defender
         $(".defender").on("click", function () {
@@ -79,13 +81,54 @@ $(document).ready(function(){
     
                 defender.remove();
                 pickedDefender = true;
-                console.log(pickedDefender);              
+                defender.removeClass("defender").addClass("myDefender");       
     
                 $("#pickedDefender").append(defender);
+
             }
         }); //closes defender picking function
     }); // closes character picking function
-        
+
+
+    $("button").on("click", function (){
+        myCharacterName =  $(".myCharacter > p").text();
+        console.log(myCharacterName);
+        myCharacterHpHolder = $(".myCharacter > span");
+        myDefenderName = $(".myDefender > p").text();
+        console.log(myDefenderName);
+        myDefenderHpHolder = $(".myDefender > span");
+        if (pickedCharacter === false && pickedDefender === false){
+            alert("You need to pick your character and defender!");
+        }
+        else if (pickedCharacter === true && pickedDefender === false) {
+            alert("You need to pick your defender!");
+        }
+        else if (pickedCharacter === true && pickedDefender === true) {
+            myHp = parseInt(myCharacterHpHolder.text()); //dragging player's hp number from the string
+            myAttack = characters[myCharacterName]["Attack Power"];
+            defenderHp = parseInt(myDefenderHpHolder.text()); //dragging defender's hp number from the string
+            defenderAttack = characters[myDefenderName]["Attack Power"];
+            console.log(myHp);
+            myHp -= defenderAttack; //defender attacks player
+            myCharacterHpHolder.text(myHp);
+            defenderHp -= myAttack; //player attacks defender
+            myDefenderHpHolder.text(defenderHp);
+            $("#attackInfo").text("You attacked " + myDefenderName + " for " + myAttack + " damage. " + myDefenderName + " attacked you back for " + defenderAttack + " damage.");
+            myAttack += 6;
+            characters[myCharacterName]["Attack Power"] = myAttack; //changing property's value, so we can keep increase Attack Power with each click;
+            
+
+
+            // console.log(myHp);
+            // console.log(myAttack);
+            // console.log(myGrowPower);
+            // console.log(defenderHp);
+            // console.log(defenderAttack);
+
+
+        }
+
+    }); //closes button function
 
 
 }); //closes document ready 
