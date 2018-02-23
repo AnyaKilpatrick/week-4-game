a6ab53b6f761b97824c878a35cf389223717b981
AnyaKilpatrick <hannakilpatrick@gmail.com>
2018-02-23 17:13:24 -0500

    added media queries and audio

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,72 +6,68 @@ var myDefenderHpHolder
 var myHp
 var defenderHp
 var myAttack
-var myGrowPower
 var defenderAttack
-var myGrowPower
 var character
 var defender
-var isClicked = false;
 var pickedCharacter = false;
 var pickedDefender = false;
 // Character Object
 var characters = {
-    "Lord Voldemort": {
+    "Tom Marvolo Riddle": {
         "Health Points": 180,
-        "Attack Power": 20,
+        "Attack Power": 25,
         "Counter Attack Power":19,
         "Base Attack Power":4
     },
     "Hermione Granger": {
-        "Health Points": 100,
-        "Attack Power": 15,
+        "Health Points": 140,
+        "Attack Power": 25,
         "Counter Attack Power":13,
-        "Base Attack Power":7
+        "Base Attack Power":15
     },
-    "Sirius Black": {
+    "Sirius 'Padfoot' Black": {
         "Health Points": 120,
-        "Attack Power": 18,
+        "Attack Power": 24,
         "Counter Attack Power":15,
-        "Base Attack Power":6
+        "Base Attack Power":20
     },
     "Bellatrix Lestrange": {
         "Health Points": 160,
-        "Attack Power": 25,
-        "Counter Attack Power":17,
-        "Base Attack Power":2
+        "Attack Power": 20,
+        "Counter Attack Power":14,
+        "Base Attack Power":7
     },
     enemies: 3 //there always will be 3 enemies out of 4 characters
 };
+
+var theme = function(){
+    var audio = new Audio("assets/audio/theme.mp3");
+    audio.volume = 0.2;
+    audio.play();
+}
+theme();
 // dragging values out of object properties to display them on the page
-var voldemortHp = $("#hp1");
-voldemortHp.text(characters["Lord Voldemort"]["Health Points"]);
-
-var hermioneHp = $("#hp2");
-hermioneHp.text(characters["Hermione Granger"]["Health Points"]);
-
-var siriusHp = $("#hp3");
-siriusHp.text(characters["Sirius Black"]["Health Points"]);
-
-var bellatrixHp = $("#hp4");
-bellatrixHp.text(characters["Bellatrix Lestrange"]["Health Points"]);
+$("#hp1").text(characters["Tom Marvolo Riddle"]["Health Points"]);
+$("#hp2").text(characters["Hermione Granger"]["Health Points"]);
+$("#hp3").text(characters["Sirius 'Padfoot' Black"]["Health Points"]);
+$("#hp4").text(characters["Bellatrix Lestrange"]["Health Points"]);
 
 
 $(document).ready(function(){
 
     $(".character").on("click", function (){
 
-
         // Picking a Character
         if (pickedCharacter === false) {
             character = $(this); //grabbing a clicked div
 
             character.remove(); //remove it from original location
-            pickedCharacter = true; //set var pickedCharacter to "true"
+            pickedCharacter = true;
             character.removeClass("character").addClass("myCharacter"); //changing class name for our picked character
             
 
-            $("#pickedUser").append(character); // insert removed div to "You Character" div
-            $(".character").removeClass("character").addClass("defender"); //changing class name of left divs, so we can treat them differently later and set different style
+            $("#pickedUser").append(character);
+            $(".character").removeClass("character").addClass("defender");
             $("#pick").text("Pick Your Defender");    
         }
         // Picking a defender
@@ -84,6 +80,7 @@ $(document).ready(function(){
                 defender.removeClass("defender").addClass("myDefender");       
     
                 $("#pickedDefender").append(defender);
+                $("#pick").text("Available Enemies"); 
 
             }
         }); //closes defender picking function
@@ -114,6 +111,7 @@ $(document).ready(function(){
             myAttack += characters[myCharacterName]["Base Attack Power"];
             characters[myCharacterName]["Attack Power"] = myAttack; 
             //changing property's value, so we can keep increase Attack Power by Base Attack Power with each click;
+
             if ( myCharacterHpHolder.text() <= 0) { //if my HP falls below 0
                 $("#attackInfo").text("You've been defeated...GAME OVER!");
                 var resetBtn = $("<button>");
@@ -132,7 +130,7 @@ $(document).ready(function(){
                 var enemiesLeft = characters.enemies - 1;
                 characters.enemies = enemiesLeft;
                 console.log("Enemies left: " + characters.enemies);
-                if (characters.enemies === 0) {
+                if (characters.enemies === 0) { //when there are no more enemies left
                     $("#attackInfo").text("YOU WIN!!!");
                     var resetBtn = $("<button>");
                     resetBtn.addClass("btn btn-secondary");
@@ -145,8 +143,6 @@ $(document).ready(function(){
                 }
                 pickedDefender = false;
             }
-
-
         }
     }); //closes button function
 }); //closes document ready 
